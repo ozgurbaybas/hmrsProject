b1c0e819dda3310204d3f8978b698db8493b753b
Özgür Baybaş <ozgurbaybas@gmail.com>
2022-01-17 20:28:39 +0300

    Returned messages as a result of the request

---
+++ src/main/java/ozgurbaybas/hrmsProject/business/constants/Messages.java
@@ -0,0 +1,16 @@
+package ozgurbaybas.hrmsProject.business.constants;
+
+public class Messages {
+
+    public static String Added = "Added!";
+    public static String Listed = "Listed!";
+    public static String Updated = "Updated!";
+    public static String Successful = "Created Successfully!";
+    public static String RegistrationError = "If you are a member, please register with Gmail, Hotmail or Outlook e-mail or if you are an employer, please enter your company e-mail information.";
+    public static String Required = "Please do not leave any blank spaces and try to enter your passwords correctly.";
+    public static String CannotRepeat = "The information you want to add cannot be repeated. Please check.";
+    public static String IdentityNumberFailed = "Please your citizenship number must be 11 digits or you must be a real user.";
+    public static String JobPositionCannotRepeat = "There is a job position you want to add.";
+
+}
+
