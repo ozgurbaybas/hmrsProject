3332fad4669574c57730bed80ef2d5a0fad7d7f8
Özgür Baybaş <ozgurbaybas@gmail.com>
2022-01-17 15:53:17 +0300

    Setup file created

---
+++ src/main/java/ozgurbaybas/hrmsProject/HmrsProjectApplication.java
@@ -0,0 +1,13 @@
+package ozgurbaybas.hrmsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmrsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmrsProjectApplication.class, args);
+	}
+
+}
