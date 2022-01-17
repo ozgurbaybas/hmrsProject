7618bda7790ec7f83222a7023a6dba520ef77ab7
Özgür Baybaş <ozgurbaybas@gmail.com>
2022-01-18 01:02:18 +0300

    City entities file created

---
+++ src/main/java/ozgurbaybas/hrmsProject/entities/concretes/City.java
@@ -0,0 +1,33 @@
+package ozgurbaybas.hrmsProject.entities.concretes;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Data
+@Entity
+@Table(name = "cities")
+@AllArgsConstructor
+@NoArgsConstructor
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id")
+    private int cityId;
+
+    @Column(name = "city_name")
+    private String cityName;
+
+    @Column(name = "create_date")
+    private Date createDate;
+
+    @Column(name = "active")
+    private boolean active;
+
+}
