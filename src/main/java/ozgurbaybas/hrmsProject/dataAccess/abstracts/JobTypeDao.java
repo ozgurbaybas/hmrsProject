f9e04e63927fd422fa3d51c9cc3e660c24fb6390
Özgür Baybaş <ozgurbaybas@gmail.com>
2022-01-18 14:01:58 +0300

    JobType Data Access Object file created

---
+++ src/main/java/ozgurbaybas/hrmsProject/dataAccess/abstracts/JobTypeDao.java
@@ -0,0 +1,8 @@
+package ozgurbaybas.hrmsProject.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ozgurbaybas.hrmsProject.entities.concretes.JobType;
+
+public interface JobTypeDao extends JpaRepository <JobType, Integer> {
+
+}
