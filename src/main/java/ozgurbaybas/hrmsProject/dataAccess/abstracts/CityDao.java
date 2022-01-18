3ebe53fc1887d31f069ee975b77810dda1132c70
Özgür Baybaş <ozgurbaybas@gmail.com>
2022-01-18 11:29:07 +0300

    City Data Access Object file created

---
+++ src/main/java/ozgurbaybas/hrmsProject/dataAccess/abstracts/CityDao.java
@@ -0,0 +1,8 @@
+package ozgurbaybas.hrmsProject.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ozgurbaybas.hrmsProject.entities.concretes.City;
+
+public interface CityDao extends JpaRepository <City, Integer> {
+
+}
